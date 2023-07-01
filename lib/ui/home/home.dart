0d1da42a575b778bbb5c73e27d24d7572b45b739
SAHN net <sahnnet.working@gmail.com>
2023-07-01 19:57:55 +0330

    Add bannerSlider to homeScreen

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nike_ecommerce/data/repository/banner_repository.dart';
 import 'package:nike_ecommerce/data/repository/product_repository.dart';
 import 'package:nike_ecommerce/ui/home/bloc/home_bloc.dart';
+import 'package:nike_ecommerce/ui/widgets/slider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -23,13 +24,21 @@ class HomeScreen extends StatelessWidget {
             if (state is HomeSuccess) {
               return ListView.builder(
                   itemCount: 5,
-                  padding: const EdgeInsets.fromLTRB(12, 12, 12, 100),
                   itemBuilder: (context, index) {
                     switch (index) {
                       case 0:
-                        return Image.asset(
-                          'assets/images/nike_logo.png',
-                          height: 32,
+                        return Container(
+                          height: 56,
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            'assets/images/nike_logo.png',
+                            height: 24,
+                            fit: BoxFit.fitHeight,
+                          ),
+                        );
+                      case 2:
+                        return BannerSlider(
+                          banners: state.banners,
                         );
                       default:
                         return Container();
---
+++ lib/ui/widgets/image.dart
@@ -0,0 +1,26 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class ImageLoadingService extends StatelessWidget {
+  final String imageUrl;
+  final BorderRadius? borderRadius;
+
+  const ImageLoadingService({
+    Key? key,
+    required this.imageUrl, this.borderRadius,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: borderRadius,
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        fit: BoxFit.cover,
+        progressIndicatorBuilder: (context, url, downloadProgress) =>
+            CircularProgressIndicator(value: downloadProgress.progress),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+      ),
+    );
+  }
+}
