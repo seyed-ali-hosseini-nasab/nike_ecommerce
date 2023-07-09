f57679a745db8c707c9a3a2a040211f1b1299ae0
SAHN net <sahnnet.working@gmail.com>
2023-07-09 18:06:29 +0330

    ➕ Add comment entity, repository and data source

---
+++ lib/data/repository/comment_repository.dart
@@ -0,0 +1,20 @@
+import 'package:nike_ecommerce/common/http_client.dart';
+import 'package:nike_ecommerce/data/comment.dart';
+import 'package:nike_ecommerce/data/source/comment_data_source.dart';
+
+final commentRepository =
+    CommentRepository(CommentRemoteDataSource(httpClient));
+
+abstract class ICommnetRepository {
+  Future<List<CommentEntity>> getAll({required int productId});
+}
+
+class CommentRepository implements ICommnetRepository {
+  final ICommentDataSource dataSource;
+
+  CommentRepository(this.dataSource);
+
+  @override
+  Future<List<CommentEntity>> getAll({required int productId}) =>
+      dataSource.getAll(productId: productId);
+}
